4319899769ccecd1d2660d398918ea0de9d6632e
S-kudu <kudusumeyye@gmail.com>
2025-05-25 18:38:55 +0300

    Update app.py

--- 1.Hafta/app.py
+++ 1.Hafta/app.py
@@ -234,4 +234,4 @@ def export_personel_to_json():
 
 import os
 if __name__ == '__main__': 
-    app.run(host=0.0.0.0, port =int(os.environ.get("POST",5000)))
+    app.run(host=0.0.0.0, port =int(os.environ.get("PORT",5000)))
